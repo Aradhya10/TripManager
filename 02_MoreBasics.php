c20c86ef542f498acdc6c270e6728e3bcfc06205
Aradhya Ranjan <aradhya10fcb@gmail.com>
2023-03-20 17:49:10 +0530

    Php project

---
+++ 02_MoreBasics.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+  .container{
+    max-width:80%;
+    background-color:grey;
+    margin: auto;
+  }
+</style>
+<body>
+    <div class="container">
+        <h1>Lets learn PHP</h1>
+        This is a container 
+        <?php
+        ?>
+</div>
+</body>
+</html>
